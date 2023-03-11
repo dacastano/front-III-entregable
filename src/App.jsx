74d47054720cd219faf3df22990aa11b5ad0b5ae
Diego Castano <dacastanoa@unal.edu.co>
2023-03-10 20:16:08 -0500

    Commiting entregable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import React, { useState } from 'react';
+import Card from './Card';
+
 function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+  const [carTypeValue, setCarTypeValue] = useState('');
+  const [serialNumberValue, setSerialNumberValue] = useState('');
+
+  const handleCarTypeChange = (event) => {
+    setCarTypeValue(event.target.value);
+  };
+
+  const handleSerialNumberChange = (event) => {
+    setSerialNumberValue(event.target.value);
+  };
+
+  const handleSubmit = (formData) => {
+    console.log(formData);
+    setCarTypeValue('');
+    setSerialNumberValue('');
+  };
 
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1>Check The Car's Availability by Serial Number</h1>
+      <Card
+        carTypeValue={carTypeValue}
+        serialNumberValue={serialNumberValue}
+        onCarTypeChange={handleCarTypeChange}
+        onSerialNumberChange={handleSerialNumberChange}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,70 @@
-//Este componente deberia recibir por props y mostrar en pantalla la informacion
-//que envia el usuario
+import React, { useState } from 'react';
 
-function Card() {
+function Card(props) {
+  const [carTypeValue, setCarTypeValue] = useState('');
+  const [serialNumberValue, setSerialNumberValue] = useState('');
+  const [validMessage, setValidMessage] = useState('');
+  const [invalidMessage, setInvalidMessage] = useState('');
+
+  const handleCarTypeChange = (event) => {
+    setCarTypeValue(event.target.value);
+  };
+
+  const handleSerialNumberChange = (event) => {
+    setSerialNumberValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (carTypeValue.trim().length > 3) {
+      setValidMessage('');
+      setInvalidMessage('Car type should be at most 3 characters long');
+      return;
+    }
+
+    if (serialNumberValue.trim().length < 6) {
+      setValidMessage('');
+      setInvalidMessage('Serial # should be at least 6 characters long');
+      return;
+    }
+
+    setInvalidMessage('');
+    setValidMessage('The car you are looking for is available');
+
+    const formData = {
+      carType: carTypeValue,
+      serialNumber: serialNumberValue,
+    };
+    props.onSubmit(formData);
+    setCarTypeValue('');
+    setSerialNumberValue('');
+  };
+
   return (
-    <div>
-      <h2>Esto es un componente</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="carType">Car Type Code:</label>
+        <input
+          type="text"
+          id="carType"
+          value={carTypeValue}
+          onChange={handleCarTypeChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="serialNumber">Serial #:</label>
+        <input
+          type="text"
+          id="serialNumber"
+          value={serialNumberValue}
+          onChange={handleSerialNumberChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+      <p style={{ color: 'red' }}>{invalidMessage}</p>
+      <p style={{ color: 'green' }}>{validMessage}</p>
+    </form>
   );
 }
 
